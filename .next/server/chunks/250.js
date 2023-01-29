ba6dca2dd6686af8e1418fa4dc20594877db195d
EricTan2002 <tanyouming1@gmail.com>
2023-01-29 14:00:59 +1100

    static output

---
+++ .next/server/chunks/250.js
@@ -0,0 +1,57 @@
+"use strict";
+exports.id = 250;
+exports.ids = [250];
+exports.modules = {
+
+/***/ 250:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(9816);
+/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(styled_jsx_style__WEBPACK_IMPORTED_MODULE_1__);
+
+
+const YEAR = new Date().getFullYear();
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
+    footer: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("footer", {
+        className: "jsx-eab45abfc5c50ce4",
+        children: [
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("small", {
+                className: "jsx-eab45abfc5c50ce4",
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("time", {
+                        className: "jsx-eab45abfc5c50ce4",
+                        children: YEAR
+                    }),
+                    " \xa9 Youming Tan.",
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                        id: "social-media",
+                        href: "https://github.com/youming16",
+                        className: "jsx-eab45abfc5c50ce4",
+                        children: "Github"
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                        id: "social-media",
+                        href: "https://twitter.com/Youming_Tan",
+                        className: "jsx-eab45abfc5c50ce4",
+                        children: "Twitter"
+                    })
+                ]
+            }),
+            react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((styled_jsx_style__WEBPACK_IMPORTED_MODULE_1___default()), {
+                id: "eab45abfc5c50ce4",
+                children: "footer.jsx-eab45abfc5c50ce4{margin-top:20rem}#social-media.jsx-eab45abfc5c50ce4{float:right;margin:5px 5px}"
+            })
+        ]
+    })
+});
+
+
+/***/ })
+
+};
+;
