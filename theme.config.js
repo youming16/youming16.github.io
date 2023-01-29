eedf05fcd35aed551ff0f413b410cbfb0d230cba
EricTan2002 <tanyouming1@gmail.com>
2023-01-29 14:24:00 +1100

    updayed next.js

---
+++ theme.config.js
@@ -0,0 +1,22 @@
+const YEAR = new Date().getFullYear()
+
+export default {
+  footer: (
+    <footer>
+      <small>
+        <time>{YEAR}</time> © Youming Tan.
+        <a id="social-media"href="https://github.com/youming16">Github</a>
+        <a id="social-media" href="https://twitter.com/Youming_Tan">Twitter</a>
+      </small>
+      <style jsx>{`
+        footer {
+          margin-top: 0px;
+        }
+        #social-media {
+          float: right;
+          margin: 5px 5px;
+        }
+      `}</style>
+    </footer>
+  ),
+}
